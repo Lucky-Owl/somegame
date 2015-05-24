24a42bbb44926b75d187a0124d06cbe4ad5049a3
Lucky-Owl <koros-anastasiya@ya.ru>
2015-05-24 18:11:43 +0300

    Update main.cpp

--- quest/main.cpp
+++ quest/main.cpp
@@ -2,14 +2,5 @@
 
 int main ( )
 {
-  room_t room( "test", "text test", true );
-  room_t room2 ("test2", "text test2", false);
-  // room.add_neighbour ( &room2 );
-  item_t item ( "test", "description test", true, false );
-  //room.add_item ( item );
-  //room.show_room ( );
-  room.change_available ( );
-  room.check ( );
-  //room.remove_item ( item);
   return 0;
 }
